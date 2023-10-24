61f7ac676258c7b01cd9631bf43ff72d1357869c
ealvar13 <erik@velascommerce.com>
2023-10-23 23:00:51 -0400

    Added front end for storing lightning address

---
+++ includes/js/hdq_a_light_script.js
@@ -0,0 +1,9 @@
+function validateEmail(event) {
+    const email = document.getElementById("lightning_address").value;
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    
+    if (!emailRegex.test(email)) {
+        alert("Please enter a valid lightning address format.");
+        event.preventDefault(); // Stop the form submission
+    }
+}
--- includes/lightning_address.php
+++ includes/lightning_address.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Lightning Address Add-On: Display functionality on the quiz start.
+ * Lightning Address Add-On: Get User's Lighting Address on the quiz start.
  */
 
 // Exit if accessed directly.
@@ -8,12 +8,24 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
+// Enqueue the JavaScript file
+function hdq_enqueue_lightning_script() {
+    $script_path = plugin_dir_url( __FILE__ ) . 'js/hdq_a_light_script.js';
+
+    wp_enqueue_script('hdq-lightning-script', $script_path, array('jquery'), '1.0.0', true);
+}
+add_action('wp_enqueue_scripts', 'hdq_enqueue_lightning_script');
+
 /**
- * Display 'Hello World' at the start of the quiz.
+ * Display a user input form to collect the Lightning Address at the start of the quiz.
  */
-function la_hello_world_on_quiz_start() {
-    echo 'Hello World';
+function la_input_lightning_address_on_quiz_start() {
+    echo '<div class="hdq_row">';
+    echo '<label for="lightning_address">Enter your Lightning Address: </label>';
+    echo '<input type="text" id="lightning_address" name="lightning_address" placeholder="bolt@lightning.com">';
+    echo '<input type="submit" class="hdq_button2" id="hdq_save_settings" value="SAVE" style="margin-left:10px;" onclick="validateEmail(event);">';
+    echo '</div>';
 }
 
 // Attach our function to the 'hdq_before' action.
-add_action('hdq_before', 'la_hello_world_on_quiz_start');
+add_action('hdq_before', 'la_input_lightning_address_on_quiz_start');
