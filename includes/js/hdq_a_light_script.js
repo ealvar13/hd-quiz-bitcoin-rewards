7c024b14263dc4ff7ab33e456b165e6c3178f6d6
ealvar13 <erik@velascommerce.com>
2023-10-27 15:23:14 -0400

    Added validation for lightning address

--- includes/js/hdq_a_light_script.js
+++ includes/js/hdq_a_light_script.js
@@ -1,4 +1,41 @@
-function validateLightningAddress(event) {
+function getPayUrl(email) {
+    try {
+        const parts = email.split('@');
+        const domain = parts[1];
+        const username = parts[0];
+        const transformUrl = `https://${domain}/.well-known/lnurlp/${username}`;
+        return transformUrl;
+    } catch (error) {
+        console.error("Exception, possibly malformed LN Address:", error);
+        return null;
+    }
+}
+
+async function getUrl(path) {
+    try {
+        const response = await fetch(path);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Failed to fetch from the URL:", error);
+        return null;
+    }
+}
+
+async function validateLightningAddressWithUrl(email) {
+    const transformUrl = getPayUrl(email);
+    const responseData = await getUrl(transformUrl);
+
+    if (responseData && responseData.tag === "payRequest") {
+        console.log("Valid Lightning Address!");
+        return true;
+    } else {
+        console.log("Invalid or Inactive Lightning Address.");
+        return false;
+    }
+}
+
+async function validateLightningAddress(event) {
     const email = document.getElementById("lightning_address").value;
     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     
@@ -6,9 +43,17 @@ function validateLightningAddress(event) {
         alert("Please enter a valid lightning address format.");
         event.preventDefault(); // Stop the form submission
     } else {
+        const isValidLightningAddress = await validateLightningAddressWithUrl(email);
+
+        if (!isValidLightningAddress) {
+            alert("Invalid or Inactive Lightning Address.");
+            event.preventDefault(); // Stop the form submission
+            return;
+        }
+
         // If it's a valid lightning address, make the AJAX call
         jQuery.ajax({
-            url: my_ajax_object.ajaxurl, // This will be enqueued in the next step
+            url: my_ajax_object.ajaxurl,
             type: 'POST',
             data: {
                 action: 'store_lightning_address',
