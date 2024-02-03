dcec8a277245365a7d393a9928923402f56d72a2
Sudhanshu Dubey <sudhanshu0404@gmail.com>
2024-02-03 15:09:12 +0530

    added confetti and fixed bugs

--- bitcoin-mastermind-rewards/includes/js/bitc_a_light_script.js
+++ bitcoin-mastermind-rewards/includes/js/bitc_a_light_script.js
@@ -173,6 +173,39 @@ function sendPaymentRequest(bolt11, quizID, lightningAddress) {
         if ((data && data.success && data.details && data.details.payment_preimage) || 
             (data && data.details && data.details.status === "Complete")) {
             console.log('Payment Successful:', data.details);
+
+            const duration = 15 * 1000,
+                  animationEnd = Date.now() + duration,
+                  defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+                function randomInRange(min, max) {
+                  return Math.random() * (max - min) + min;
+                }
+
+                const interval = setInterval(function() {
+                  const timeLeft = animationEnd - Date.now();
+
+                  if (timeLeft <= 0) {
+                    return clearInterval(interval);
+                  }
+
+                  const particleCount = 50 * (timeLeft / duration);
+
+                  // since particles fall down, start a bit higher than random
+                  confetti(
+                    Object.assign({}, defaults, {
+                      particleCount,
+                      origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+                    })
+                  );
+                  confetti(
+                    Object.assign({}, defaults, {
+                      particleCount,
+                      origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+                    })
+                  );
+                }, 250);
+
             return { success: true, data: data.details };
         } else {
             console.log('Payment Not Successful:', data.details || data);
@@ -363,7 +396,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-                    fetch(`/wp-json/hdq/v1/sats_per_answer/${quizID}`)
+                    fetch(`/wordpress/wp-json/hdq/v1/sats_per_answer/${quizID}`)
                     .then(response => response.json())
                     .then(data => {
                         satsPerCorrect = parseInt(data.sats_per_correct_answer, 10);
@@ -384,7 +417,15 @@ document.addEventListener("DOMContentLoaded", function() {
                                     .then(paymentResponse => {
                                         paymentSuccessful = paymentResponse.success;
                                         satoshisToSend = paymentSuccessful ? totalSats : 0;
-                                        jQuery('#step-result').addClass('active-step').text(paymentSuccessful ? 'Payment Successful! Enjoy your free sats.' : 'Payment Failed');
+                                       if(satoshisToSend!=0){
+
+                                            jQuery('#step-result').addClass('active-step').text(paymentSuccessful ? 'Payment Successful! Enjoy your free sats.' : 'Payment Failed');
+
+                                       }else{
+                                            jQuery('#step-result').addClass('active-step').text('What went wrong? Don’t worry you still have X more tries to get it right!');
+
+                                       }
+                                       
                                         jQuery('#step-reward').addClass('active-step');
 
                                         saveQuizResults(email, scoreText, totalSats, quizName, paymentSuccessful ? 1 : 0, satoshisToSend, quizID, results_details_selections);
