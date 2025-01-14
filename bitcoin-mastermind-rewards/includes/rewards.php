46eaddc2856d018f15a3c345c373e05f1a6c6a6c
ealvar13 <erik@velascommerce.com>
2025-01-14 08:52:02 -0400

    Fixed settings saving bug

--- bitcoin-mastermind-rewards/includes/rewards.php
+++ bitcoin-mastermind-rewards/includes/rewards.php
@@ -6,19 +6,18 @@
 
 // Process form submission
 if (isset($_POST['bitc_rewards_save'])) {
-    $bitc_nonce = $_POST['bitc_about_options_nonce'];
+    $bitc_nonce = isset($_POST['bitc_rewards_nonce']) ? $_POST['bitc_rewards_nonce'] : null;
 
     // Check if nonce is valid
-    if (wp_verify_nonce($bitc_nonce, 'bitc_about_options_nonce') !== false) {
+    if ($bitc_nonce && wp_verify_nonce($bitc_nonce, 'bitc_rewards_nonce')) {
         // Loop through each quiz to save its settings
         $quizzes = fetch_all_quizzes();
         foreach ($quizzes as $quiz) {
             $quiz_id = $quiz['id'];
-
             // Fetch and sanitize checkbox value
             $reward_enabled_field = "enable_bitcoin_reward_for_" . $quiz_id;
             $reward_enabled_value = isset($_POST[$reward_enabled_field]) ? 'yes' : 'no';
-            
+
             // Fetch and sanitize sats per answer value
             $sats_field = "sats_per_answer_for_" . $quiz_id;
             $sats_value = isset($_POST[$sats_field]) ? sanitize_text_field($_POST[$sats_field]) : "";
@@ -31,12 +30,14 @@ if (isset($_POST['bitc_rewards_save'])) {
             $max_budget_field = "max_satoshi_budget_for_" . $quiz_id;
             $max_budget_value = isset($_POST[$max_budget_field]) ? sanitize_text_field($_POST[$max_budget_field]) : "";
 
-            // Now save these values to the database.
+            // Save these values to the database
             update_option($reward_enabled_field, $reward_enabled_value);
             update_option($sats_field, $sats_value);
             update_option($retries_field, $retries_value);
             update_option($max_budget_field, $max_budget_value);
         }
+    } else {
+        wp_die('Nonce verification failed for Rewards Settings.');
     }
 }
 
